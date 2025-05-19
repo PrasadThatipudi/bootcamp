b9e4acb492821a136ededc7f1e115d5ea3be2229
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-19 10:52:51 +0530

    Adds Rectangle class and area method

---
+++ Test/Math/RectangleTest.java
@@ -0,0 +1,28 @@
+package Math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+    @Test
+    void initiateRectangle() {
+        Rectangle rectangle = new Rectangle(10, 20);
+
+        assertEquals("Rectangle{width=10, height=20}", rectangle.toString());
+    }
+
+    @Test
+    void areaOfRectangle() {
+        Rectangle rectangle = new Rectangle(10, 20);
+
+        assertEquals(200, rectangle.area());
+    }
+
+    @Test
+    void areaOfSquare() {
+        Rectangle square = new Rectangle(10, 10);
+
+        assertEquals(100, square.area());
+    }
+}
---
+++ src/Math/Rectangle.java
@@ -0,0 +1,23 @@
+package Math;
+
+public class Rectangle {
+    private final int width;
+    private final int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    public int area() {
+        return width * height;
+    }
+}

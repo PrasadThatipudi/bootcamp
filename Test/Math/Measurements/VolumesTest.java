a60d7bda70438018718ad2e9ff1f97d7eab00d13
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-20 11:33:14 +0530

    Refactors Volume

--- Test/Math/Measurements/VolumesTest.java
+++ Test/Math/Measurements/VolumesTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class VolumeTest {
+class VolumesTest {
     @Test
     void initiateLiter() throws InvalidMeasurementException {
         Volume liter = Volume.createLiter(10);
--- src/Math/Measurements/Volume.java
+++ src/Math/Measurements/Volume.java
@@ -5,9 +5,13 @@ import java.util.Objects;
 public class Volume {
 
     private final double liters;
+    
+    public Volume(double liters) {
+        this.liters = liters;
+    }
 
     public static Volume createGallon(double gallons) throws InvalidMeasurementException {
-        if(gallons < 0) {
+        if(isNegative(gallons)) {
             throw new InvalidMeasurementException();
         }
         return new Volume(gallons * 3.78);
@@ -24,14 +28,14 @@ public class Volume {
         return Objects.hashCode(liters);
     }
 
-    public Volume(double liters) {
-        this.liters = liters;
-    }
-
     public static Volume createLiter(double liters) throws InvalidMeasurementException {
-        if(liters < 0){
+        if(isNegative(liters)){
             throw new InvalidMeasurementException();
         }
         return new Volume(liters);
+    }
+
+    private static boolean isNegative(double liters) {
+        return liters < 0;
     }
 }
